033f431d554418c65f2720de1dd2d3dbd2528001
Apple <apple@Fideles-Mac-Pro.attlocal.net>
2020-08-09 17:27:59 -0500

    initial commit node practice

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,29 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
-
-}
-
+    if(num%2==0){
+     return(true)
+    }else{
+      return(false)
+    }
+  } 
+  
 const sum = (arr) => {
   // write code for numbers.sum
-
+   const reducer = (a,b) => a+b
+   return arr.reduce(reducer)
+    
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
 
+  for (var i=0; i<arr.length; i++){ 
+
+  let totals = sum += arr[i]
+  if(totals == sum){
+    return false
+  }
+  
+  }
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
+  let x = str.split(" ",0)
+  return x
+  let y = str.split(" ",1)
+  return y
+  let z = str.split(" ",2)
+  return z
+
 
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+return str.match(/(..)/g)
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let x = str.split('').reverse().join('')
+  return x
 }
 
 module.exports = {
